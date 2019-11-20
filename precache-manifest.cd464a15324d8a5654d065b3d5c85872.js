2b919a84a2ef5a1f4719029d627fcbb79a306ae4
Mortzcent <moryckuba@gmail.com>
2019-11-20 14:42:58 +0100

    Updates

---
+++ precache-manifest.cd464a15324d8a5654d065b3d5c85872.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b1335f486b8301db2b1002c9d8ea1d",
+    "url": "/pokedex-languages-api-docs/./index.html"
+  },
+  {
+    "revision": "305b404ae167efa21660",
+    "url": "/pokedex-languages-api-docs/./static/css/main.dbe2cfd0.chunk.css"
+  },
+  {
+    "revision": "256a3cb391d16f710e42",
+    "url": "/pokedex-languages-api-docs/./static/js/2.7c5903a4.chunk.js"
+  },
+  {
+    "revision": "305b404ae167efa21660",
+    "url": "/pokedex-languages-api-docs/./static/js/main.7a30d084.chunk.js"
+  },
+  {
+    "revision": "c293aed70f0652e1bf21",
+    "url": "/pokedex-languages-api-docs/./static/js/runtime-main.e7bb4cc4.js"
+  },
+  {
+    "revision": "7f8455b70734a87596991e1f24053edc",
+    "url": "/pokedex-languages-api-docs/./static/media/china.7f8455b7.svg"
+  },
+  {
+    "revision": "968aaa24eeffdd779e4fc9c27c5cb730",
+    "url": "/pokedex-languages-api-docs/./static/media/france.968aaa24.svg"
+  },
+  {
+    "revision": "d810f62137d64a0a58035c80489b7d54",
+    "url": "/pokedex-languages-api-docs/./static/media/germany.d810f621.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/pokedex-languages-api-docs/./static/media/github-logo.76d344f0.svg"
+  },
+  {
+    "revision": "612e617f5d72975e63e53d793d757785",
+    "url": "/pokedex-languages-api-docs/./static/media/italy.612e617f.svg"
+  },
+  {
+    "revision": "1f905d23af144bc76e488f00d516de2e",
+    "url": "/pokedex-languages-api-docs/./static/media/japan.1f905d23.svg"
+  },
+  {
+    "revision": "7a910bbe52cb317b28e4cf7bf7750281",
+    "url": "/pokedex-languages-api-docs/./static/media/sign-in.7a910bbe.svg"
+  },
+  {
+    "revision": "8a067f19c6a32f781b8cb971a3214629",
+    "url": "/pokedex-languages-api-docs/./static/media/south-korea.8a067f19.svg"
+  },
+  {
+    "revision": "36938bbe277961386e7e309dcb30cbd8",
+    "url": "/pokedex-languages-api-docs/./static/media/spain.36938bbe.svg"
+  },
+  {
+    "revision": "e5564902e2642c5e6e2e98e68a7d41f5",
+    "url": "/pokedex-languages-api-docs/./static/media/united-kingdom.e5564902.svg"
+  }
+]);
