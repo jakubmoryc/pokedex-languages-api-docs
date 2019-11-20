7b9603951a0d6f7ba387ba0c31dc26eb24eaaee4
Mortzcent <moryckuba@gmail.com>
2019-11-20 14:49:34 +0100

    changed github link to match

--- src/components/GitHubButton/GitHubButton.js
+++ src/components/GitHubButton/GitHubButton.js
@@ -5,7 +5,7 @@ import github from '../../icons/github-logo.svg'
 export default function GitHubButton() {
     return (
             <a  className="github-button"  
-                href="https://github.com/jakubmoryc/pokelang-rest-api-documentation" 
+                href="https://github.com/jakubmoryc/pokedex-languages-api-docs" 
                 target="_blank" rel="noopener noreferrer" 
                 title="See code on GitHub"
             >
