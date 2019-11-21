b8b70bb68a127a1817a7b28ac83d843a1f021cba
Mortzcent <moryckuba@gmail.com>
2019-11-21 10:28:03 +0100

    fixing react router

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './css/app.css';
 import Navbar from './components/Navbar/Navbar';
 
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route,
   Redirect
@@ -75,7 +75,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router basename=''>
         <Navbar 
             setActivePage={this.setActivePage}
             activePage={this.state.activePage}
